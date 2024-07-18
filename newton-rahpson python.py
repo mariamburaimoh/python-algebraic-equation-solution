9b1667719d33b575df979580631825e4fcb9cb41
mariamburaimoh <127381951+mariamburaimoh@users.noreply.github.com>
2024-07-18 13:07:13 +0100

    newton-rahpson python final version.py

--- newton-rahpson python.py
+++ newton-rahpson python.py
@@ -1,34 +1,15 @@
-num1 = eval(input("enter coefficient for x^3: " ))
-num2 = eval(input("enter coefficient for x^2: " ))
-num3 = eval(input("enter coefficient for x: " ))
-num4 = eval(input("enter constant: " ))
-num5 = eval(input("enter a: "))
-num6 = eval(input("enter tolerable error: "))
+#define function for Newton Raphson Table
+def NewtonRaphsonTable():
+    #print value table
+     print("NEWTON RAPHSON TABLE")
+     print("n       a            f(a)         f'(a)        -f(a)/f'(a)       Xn")
 
-def f(x):
-     result = (((num1) * (x ** 3)) + ((num2) * (x ** 2)) + ((num3) * x) + num4)
-     return result
-
-def g(x):
-     result = (3 * num1 * (x ** 2)) + (2 * num2 * x) + num3
-     return result
-
-def h(x):
-     result = (f(x)/g(x))
-     return result
-
-
-if((g(num5) == 0)):
-     print("error, pick another number")
-else:
-     print("NEWTON RAHPSON TABLE")
-     print("n       a            f(a)         f'(a)         f(a)/g(a)       Xn")
-
+     #declare initial values for variables
      a = num5
      fa = f(a)
      ga = g(a)
      ha = h(a)
-     xn = a - ha
+     xn = a + ha
      fxn = f(xn)
      gxn = g(xn)
      hxn = h(xn)
@@ -37,17 +18,52 @@ else:
      print("%d     %f     %f      %f     %f     %f"%(i,a,fa,ga,ha,xn))
      i = i + 1
      
+     #set condition for changing values of variables
      while (abs(fxn) > error):
                a = xn
                fa = fxn
                ga = gxn
                ha = hxn
-               xn = (a - ha)
+               xn = (a + ha)
                fxn = f(xn)
                gxn = g(xn)
                hxn = h(xn)
                print("%d     %f     %f      %f     %f     %f"%(i,a,fa,ga,ha,xn))
                i = i + 1
 
-          
-     print("The root is %f"%(xn))     
+     #print root     
+     print("The root is %f"%(xn))
+     return 
+
+#prompt user to enter coefficients for the equation to be solved
+num1 = eval(input("enter coefficient for x^3: " ))
+num2 = eval(input("enter coefficient for x^2: " ))
+num3 = eval(input("enter coefficient for x: " ))
+num4 = eval(input("enter constant: " ))
+
+#prompt user to enter in guess for root and tolerable error
+num5 = eval(input("enter a: "))
+num6 = eval(input("enter tolerable error: "))
+
+#define function of the equation, f
+def f(x):
+     result = (((num1) * (x ** 3)) + ((num2) * (x ** 2)) + ((num3) * x) + num4)
+     return result
+
+#define the differential of the function f, g
+def g(x):
+     result = (3 * num1 * (x ** 2)) + (2 * num2 * x) + num3
+     return result
+
+#define f(x)/g(x), h(x)
+def h(x):
+     result = (-(f(x)/g(x)))
+     return result
+
+#end program if g(a) = 0, continue if g(0) != 0
+if((g(num5) == 0)):
+     print("error, pick another number")
+     num5 = eval(input("enter another number for a: "))
+     NewtonRaphsonTable()
+else:
+     NewtonRaphsonTable()  
